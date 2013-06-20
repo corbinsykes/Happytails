6bedf054874471e159c6786488cc09a504bdabc5
Jamal Powell <jamalkpowell@gmail.com>
2013-06-20 16:35:27 -0400

    initial commit

---
+++ happytails.rb
@@ -0,0 +1,20 @@
+Class Person
+  attr_accessor :name, :age, :gender, :kids, :num_of_pets
+  def initialize
+    @name = name
+    @age = age
+    @gender = gender
+    @kids = kids
+    @num_of_pets = []
+  end
+end
+
+Class Animal :name, :breed, :age, :gender, :fav_toys
+  def initialize
+    @name = name
+    @breed = breed
+    @age = age
+    @gender = gender
+    @fav_toys = fav_toys
+  end
+end
